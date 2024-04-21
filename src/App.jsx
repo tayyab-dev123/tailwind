52f524a87807eb7e09b2ff8f1ee42c15433b9736
Tayyab Shahzad <101314819+tayyab-dev123@users.noreply.github.com>
2024-04-21 00:13:13 -0700

    Gird Flex Animation And Learning

--- src/App.jsx
+++ src/App.jsx
@@ -3,84 +3,65 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      {/* <h1>Colors</h1>
-      <p className="text-lime-50  ">TailwindCSS</p>
-      <p className="text-lime-100">TailwindCSS</p>
-      <p className="text-lime-300">TailwindCSS</p>
-      <p className="text-lime-400">TailwindCSS</p>
-      <p className="text-lime-500">TailwindCSS</p>
-      <p className="text-lime-600">TailwindCSS</p>
-      <p className="text-lime-700">TailwindCSS</p>
-      <p className="text-lime-800">TailwindCSS</p>
-      <p className="text-lime-900">TailwindCSS</p> */}
-      {/* <h1>Background Colors</h1>
-      <p className="bg-red-50">TailwindCSS</p>
-      <p className="bg-red-100">TailwindCSS</p>
-      <p className="bg-red-200">TailwindCSS</p>
-      <p className="bg-red-300">TailwindCSS</p>
-      <p className="bg-red-400">TailwindCSS</p>
-      <p className="bg-red-500">TailwindCSS</p>
-      <p className="bg-red-600">TailwindCSS</p>
-      <p className="bg-red-700">TailwindCSS</p>
-      <p className="bg-red-800">TailwindCSS</p>
-      <p className="bg-red-900">TailwindCSS</p> */}
-      {/* <h1>Spacing X</h1>
-      <div className="flex space-x-5">
-        <div className="bg-green-300 m-2 p-3">1</div>
-        <div className="bg-green-300 m-2 p-3">2</div>
-        <div className="bg-green-300 m-2 p-3">3</div>
+      <div className="bg-indigo-400 h-screen w-screen sm:bg-pink-600">
+        <h2>Trasformation Transition Animation</h2>
+        <button className="m-2 p-5 text-white bg-black  hover:bg-yellow-200 hover:text-emerald-950 duration-500  transition ease-in-out">
+          Click Me
+        </button>
+        {/* Trasfor */}
+        <button
+          className="m-2 p-5 text-white bg-black  hover:bg-yellow-200
+         hover:text-emerald-950 duration-500  transition ease-in-out hover:-translate-y-1 hover:scale-100"
+        >
+          Click Me
+        </button>
+
+        {/* Animation */}
+
+        <button type="button" className="relative bg-indigo-500 p-10 m-10 mr-3">
+          <span className="absolute inset-0 flex items-center justify-center">
+            <span className="animate-spin h-5 w-5 border-t-2 border-white rounded-full"></span>
+          </span>
+          Processing...
+        </button>
+
+        {/* <button
+          type="button"
+          className="bg-indigo-500  p-10 m-10 mr-3 animate-spin"
+          disabled
+        >
+          Processing...
+        </button> */}
+
+        {/* <button
+          type="button"
+          className="bg-indigo-500  p-10 m-10 mr-3 animate-spin"
+          disabled
+        >
+          Processing...
+        </button>
+        <button
+          type="button"
+          className="bg-indigo-500  p-10 m-10 mr-3 animate-ping"
+          disabled
+        >
+          Processing...
+        </button>
+        <button
+          type="button"
+          className="bg-indigo-500  p-10 m-10 mr-3 animate-pulse"
+          disabled
+        >
+          Processing...
+        </button>
+        <button
+          type="button"
+          className="bg-indigo-500  p-10 m-10 mr-3 animate-bounce"
+          disabled
+        >
+          Processing...
+        </button> */}
       </div>
-      <h1>Spacing 7</h1>
-      <div className="flex flex-col space-y-5 w-16">
-        <div className="bg-green-300 m-2 p-3">1</div>
-        <div className="bg-green-300 m-2 p-3">2</div>
-        <div className="bg-green-300 m-2 p-3">3</div>
-      </div> */}
-      <h1>Typography</h1>
-      {/* <h2>Font size</h2>
-      <div className="container bg-teal-100 p-7 mb-10">
-        <p className="text-xs">Tailwind</p>
-        <p className="text-sm">Tailwind</p>
-        <p className="text-base">Tailwind</p>
-        <p className="text-lg">Tailwind</p>
-        <p className="text-xl">Tailwind</p>
-        <p className="text-2xl">Tailwind</p>
-        <p className="text-3xl">Tailwind</p>
-        <p className="text-4xl">Tailwind</p>
-        <p className="text-5xl">Tailwind</p>
-        <p className="text-6xl">Tailwind</p>
-        <p className="text-7xl">Tailwind</p>
-        <p className="text-8xl">Tailwind</p>
-        <p className="text-9xl">Tailwind</p>
-      </div> */}
-
-      {/* <h2>Font Weight</h2>
-      <p className="font-light">The quick brown fox ...</p>
-      <p className="font-normal ">The quick brown fox ...</p>
-      <p className="font-medium ">The quick brown fox ...</p>
-      <p className="font-semibold ">The quick brown fox ...</p>
-      <p className="font-bold">The quick brown fox ...</p> */}
-
-      {/* <h2>Font Spacing (Tracking)</h2>
-      <p className="tracking-tighter">Tailwind</p>
-      <p className="tracking-tight">Tailwind</p>
-      <p className="tracking-normal">Tailwind</p>
-      <p className="tracking-wide">Tailwind</p>
-      <p className="tracking-wider">Tailwind</p>
-      <p className="tracking-widest">Tailwind</p> */}
-      {/* <h2>Font Decoration </h2>
-      <p className="tracking-tighter  underline decoration-3 decoration-solid decoration-red-700">
-        Tailwind
-      </p>
-      <p className="tracking-tight">Tailwind</p>
-      <p className="tracking-normal">Tailwind</p>
-      <p className="tracking-wide">Tailwind</p>
-      <p className="tracking-wider">Tailwind</p>
-      <p className="tracking-widest">Tailwind</p> */}
-      {/* <h2>Text transform</h2>
-      <p className="capitalize">tailwind</p>
-      <p className="uppercase hover:lowercase">tailwind</p>
-      <p className="lowercase">tailwind</p> */}
     </>
   );
 }
